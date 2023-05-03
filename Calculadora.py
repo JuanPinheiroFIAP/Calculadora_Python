c13850f6d1c4f7cf044ed198b31d32839f01f26e
JuanPinheiroFIAP <rm552202@fiap.com.br>
2023-05-03 13:51:39 -0300

    primeiro_commit

---
+++ Calculadora.py
@@ -0,0 +1,21 @@
+import math
+print("Sejá bem vindo a minha calculadora!")
+print("Vamos calcular!")
+
+escolha = input("Você quer utilizar nossa calculadora? S/N").upper
+
+match escolha:
+    case "S":
+        print("Oi")
+
+
+
+
+
+# match :
+#     case     
+
+# adição 
+# subtração
+# divisão
+# multiplicação
