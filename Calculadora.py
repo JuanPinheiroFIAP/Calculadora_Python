faced9595a2c491d2317adb1b6aa32dd4a848624
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-03 21:47:24 -0300

    Calculadora

--- Calculadora.py
+++ Calculadora.py
@@ -1,21 +1,39 @@
 import math
 print("Sejá bem vindo a minha calculadora!")
 print("Vamos calcular!")
+print("Ok vamos lá!")
+while True:
+    print('''
+            [1] - Adição
+            [2] - Subtração
+            [3] - Divisão
+            [4] - Multiplicação
+            [5] - Exit''')
+    menu = input("Qual a opção que você quer escolher? ")
 
-escolha = input("Você quer utilizar nossa calculadora? S/N").upper
+    if menu in ('1', '2' , '3' , '4'):
+        num1 = int(input("digite seu primeiro número: "))
+        num2 = int(input("digite seu segundo número: "))
 
-match escolha:
-    case "S":
-        print("Oi")
+    elif menu in '5':
+        print("Tchau!")
+        break
 
-
-
-
-
-# match :
-#     case     
-
-# adição 
-# subtração
-# divisão
-# multiplicação
+    match menu:
+        case '1':
+            resultado = num1 + num2
+            print(f"O resultado da sua adição foi {resultado}")
+        case '2':
+            resultado = num1 - num2
+            print(f"O resultado da sua subtração foi {resultado}")
+        case '3':
+            if num2 != 0:
+                 print("Não é possivel fazer uma divisão por zero")
+            else:
+                resultado = num1 / num2
+                print(f"O resultado da sua divisão foi de {resultado}")
+        case '4':
+            resultado = num1 * num2
+            print(f"O resultado da sua multiplicação foi de {resultado}")
+        case _:
+            print("Opção invalida!")
