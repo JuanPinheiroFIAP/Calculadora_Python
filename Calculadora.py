7ebd1f31d55121647eb10c9dd66912aec608f6fb
JuanPinheiroFIAP <rm552202@fiap.com.br>
2023-05-05 20:40:13 -0300

    quarto_commit

--- Calculadora.py
+++ Calculadora.py
@@ -1,6 +1,9 @@
 import math
 from os import system, name
 from time import sleep
+
+#-----------------------------------------------------------------------------------------------------------------
+
 def clean():
     if name == 'nt':
         _ = system('cls')
@@ -8,9 +11,14 @@ def clean():
     else:
         _ = system('clear')
 
+#-----------------------------------------------------------------------------------------------------------------
+
 print("Sejá bem vindo a minha calculadora!")
 print("Vamos calcular!")
 print("Ok vamos lá!")
+
+#-----------------------------------------------------------------------------------------------------------------
+
 while True:
     print("----"*20)
     print('''
@@ -18,12 +26,13 @@ while True:
             [2] - Subtração
             [3] - Divisão
             [4] - Multiplicação
-            [5] - Funções
+            [5] - Função
             [6] - Exit''')
     print()
     print("----"*20)
     menu = input("Qual a opção que você quer escolher? ")
-    print(resultado)
+
+#-----------------------------------------------------------------------------------------------------------------
 
     if menu in ('1', '2' , '3' , '4',):
         print("----"*20)
@@ -32,8 +41,15 @@ while True:
         print("----"*20)
 
     elif menu in '6':
-        print("Tchau!")
+        print("----"*20)
+        print()
+        print("Até mais!")
+        print(9
+        )
+        print("----"*20)
         break
+    
+#-----------------------------------------------------------------------------------------------------------------
 
     match menu:
         case '1':
@@ -43,6 +59,7 @@ while True:
             print("----"*20)
             sleep(3)
             clean()
+            
         case '2':
             resultado = num1 - num2
             print("----"*20)
@@ -50,6 +67,7 @@ while True:
             print("----"*20)
             sleep(3)
             clean()
+
         case '3':
             if num2 != 0:
                  print("Não é possivel fazer uma divisão por zero")
@@ -62,6 +80,7 @@ while True:
                 print("----"*20)
                 sleep(3)
                 clean()
+
         case '4':
             resultado = num1 * num2
             print("----"*20)
@@ -69,44 +88,33 @@ while True:
             print("----"*20)
             sleep(3)
             clean()
+
         case '5':
-            print('''
-            Funções:
-            [1] - Primeiro Grau
-            [2] - Segundo Grau
-            ''')
-            escolha = input('Qual função você quer calcular?')
+            print("Vamos calcular sua função do segundo grau!")
+            a = int(input('Qual é o valor do seu "a"? '))
+            b = int(input('Qual é o valor do seu "b"?'))
+            c = int(input('Qual é o valor do seu "c"?'))
+            delta = (b**2)-(4*a*c)
 
-            if escolha == '1':
-                a = int(input('Qual é o valor do seu "a"? '))
-                b = int(input('Qual é o valor do seu "b"?'))
+            if delta < 0:
+                print("Não será possivel calcular quando o delta for negativo")
+                exit()
+                sleep(3)
+                clean()
 
-                delta = b*b - 4*a
-                print(delta)
-
-            elif escolha == "2":
-                a = int(input('Qual é o valor do seu "a"? '))
-                b = int(input('Qual é o valor do seu "b"?'))
-                c = int(input('Qual é o valor do seu "c"?'))
-                delta = (b**2)-(4*a*c)
-
-                if delta < 0:
-                    print("Não será possivel calcular quando o delta for negativo")
-                    exit()
-                    sleep(3)
-                    clean()
-
-                else :
-                    x=math.sqrt(delta)
-                    x1=(-b+x)/(2*a)
-                    x2=(-b-x)/(2*a)
-                    print("----"*20)
-                    print(f"O valor do seu x1 é ==>{x1}") 
-                    print(f"O valor do seu x2 é ==>{x2}")
-                    print("----"*20)
-                    sleep(3)
-                    clean()
+            else :
+                x=math.sqrt(delta)
+                x1=(-b+x)/(2*a)
+                x2=(-b-x)/(2*a)
+                print("----"*20)
+                print(f"O valor do seu x1 é ==>{x1}") 
+                print(f"O valor do seu x2 é ==>{x2}")
+                print("----"*20)
+                sleep(3)
+                clean()
         case _:
             print("Opção invalida!")
-            sleep(3)
+            sleep(2)
             clean()
+
+#-----------------------------------------------------------------------------------------------------------------
