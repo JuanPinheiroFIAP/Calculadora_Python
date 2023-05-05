de4371b20f80dbb452d111e783155f2c9e6c5a0c
JuanPinheiroFIAP <rm552202@fiap.com.br>
2023-05-04 22:50:11 -0300

    terceiro_commit

--- Calculadora.py
+++ Calculadora.py
@@ -2,6 +2,15 @@ import math
 from os import system, name
 from time import sleep
 
+def bhaskara():
+    raiz = math.sqrt(delta)
+    print(raiz)
+    result = -b + delta 2* a * c
+
+def função(a,b,c):
+    return(b*b - 4*a*c)
+
+
 def clean():
     if name == 'nt':
         _ = system('cls')
@@ -18,14 +27,15 @@ while True:
             [2] - Subtração
             [3] - Divisão
             [4] - Multiplicação
-            [5] - Exit''')
+            [5] - Funções
+            [6] - Exit''')
     menu = input("Qual a opção que você quer escolher? ")
 
-    if menu in ('1', '2' , '3' , '4'):
+    if menu in ('1', '2' , '3' , '4',):
         num1 = int(input("digite seu primeiro número: "))
         num2 = int(input("digite seu segundo número: "))
 
-    elif menu in '5':
+    elif menu in '6':
         print("Tchau!")
         break
 
@@ -55,6 +65,29 @@ while True:
             print(f"O resultado da sua multiplicação foi de {resultado}")
             sleep(3)
             clean()
+        case '5':
+            print('''
+            Funções:
+            [1] - Primeiro Grau
+            [2] - Segundo Grau
+            ''')
+            escolha = input('Qual função você quer calcular?')
+
+            if escolha == '1':
+                a = int(input('Qual é o valor do seu "a"? '))
+                b = int(input('Qual é o valor do seu "b"?'))
+
+                delta = b*b - 4*a
+                print(delta)
+
+            elif escolha == "2":
+                a = int(input('Qual é o valor do seu "a"? '))
+                b = int(input('Qual é o valor do seu "b"?'))
+                c = int(input('Qual é o valor do seu "c"?'))
+                delta = função(a,b,c)
+
+
+
         case _:
             print("Opção invalida!")
             sleep(3)
